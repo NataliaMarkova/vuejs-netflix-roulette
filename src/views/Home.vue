578fa9d8bce3a49290d34cc9b0ede694a9b80c34
Natalia Markova <natalia.markova@travisperkins.co.uk>
2020-09-14 21:30:41 +0300

    Homework 4: State management

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,22 @@
 <template>
   <div class="container">
-    <SearchOptions></SearchOptions>
-    <SearchResult :movies = "movies"/>
+    <SearchOptions />
+    <SearchResult />
     <Footer />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
-import { JsonToMovieConverter } from '@/services/jsonToMovieConverter';
-
-import { Movie } from '@/models/Movie';
 
 import SearchOptions from '@/components/SearchOptions.vue';
 import SearchResult from '@/components/SearchResult.vue';
 import Footer from '@/components/Footer.vue';
 
-import moviesJson from '@/assets/movies.json';
-
-const jsonToMovieConverter = new JsonToMovieConverter();
-
 @Component({
   components: { SearchOptions, SearchResult, Footer },
 })
 export default class Home extends Vue {
-  defaultMovies: Movie[] = jsonToMovieConverter.convertToMovieArray(moviesJson);
-
-  searchBy = 'title';
-
-  searchText = '';
-
-  get movies(): Movie[] {
-    return this.defaultMovies;
-  }
 }
 
 </script>
